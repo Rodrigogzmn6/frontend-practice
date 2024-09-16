b1331b76d53360fade1faa5d64efd548622c9d59
Rodrigo Guzman <rodrigo.guzman@sovos.com>
2024-09-16 15:23:21 -0300

    fix(projects/ableton): Mobile header shows all option and entire width

--- projects/ableton/src/app/components/Header.tsx
+++ projects/ableton/src/app/components/Header.tsx
@@ -21,16 +21,16 @@ export const Header = () => {
       className={`lg:bg-white lg:flex lg:flex-col lg:gap-8 lg:relative lg:text-black
         ${visibleMenu ? 'bg-blue' : 'border-b-2 closed'}  `}>
       <div
-        className={`leading flex flex-col font-semibold gap-6 items-start relative text-xl px-5 py-5
+        className={`flex flex-col font-semibold gap-6 items-start relative text-xl px-5 py-5
           lg:flex-row lg:items-center lg:px-10
           ${visibleMenu && 'text-white lg:text-black'}`}>
         <LeadingHeader handleMenu={handleMenuClick} visibleMenu={visibleMenu} />
         <div
-          className={`dropdown-menu absolute bg-blue flex flex-col gap-5 left-0 px-5 text-white top-full
+          className={`dropdown-menu absolute bg-blue flex flex-col gap-5 left-0 px-5 text-white top-full w-full
           lg:hidden
           ${visibleMenu ? 'opened' : 'closed'} `}>
           <PrimaryHeader />
-          <SecondaryHeader />
+          <SecondaryHeader visibleMenu={visibleMenu} />
         </div>
         <div
           className='hidden
--- projects/ableton/src/app/components/SecondaryHeader.tsx
+++ projects/ableton/src/app/components/SecondaryHeader.tsx
@@ -2,14 +2,17 @@ import { FromAbleton } from './FromAbleton'
 import { OnAbleton } from './OnAbleton'
 
 interface SecondaryHeaderProps {
+  visibleMenu?: boolean
   visibleSecondaryMenu?: boolean
 }
 export const SecondaryHeader: React.FC<SecondaryHeaderProps> = ({
+  visibleMenu,
   visibleSecondaryMenu
 }) => {
   return (
     <div
-      className={`hidden lg:bg-white lg:border-b-2 lg:flex lg:flex-col lg:grow lg:font-semibold lg:gap-5 lg:pb-14 lg:px-10 lg:text-3xl  lg:top-full lg:w-full
+      className={`lg:bg-white lg:border-b-2 lg:flex lg:flex-col lg:grow lg:font-semibold lg:gap-5 lg:pb-14 lg:px-10 lg:text-3xl  lg:top-full lg:w-full
+        ${visibleMenu === true ? '' : 'hidden'}
       ${visibleSecondaryMenu === true ? 'lg:absolute ' : 'lg:hidden'}`}>
       <OnAbleton />
       <FromAbleton />
